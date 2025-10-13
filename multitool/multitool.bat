ab349df6d8a65febc539b9a073e21a848e705a1d
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-10-13 21:10:28 +0200

    fix

--- multitool/multitool.bat
+++ multitool/multitool.bat
@@ -5,15 +5,17 @@
 title Multitool - by Shadowdara
 chcp 65001 >nul
 call :banner
+call :main
+exit /b
 
+:main
 echo [INFO] You can close programms with CTRL + C
 echo.
 echo 1) MSG-Formatter
 echo.
-set /p input="-------------------->" 
-if /I %input% EQU 1 start "msg-formatter/start.bat"
-
-exit /b
+set /p input="--------------------> "
+if /I %input% EQU 1 start "MSG-Formatter" "%~dp0msg-formatter\\start.bat"
+pause
 
 :banner
 echo.
