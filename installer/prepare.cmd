ec4ab954c9eb1424ea4726724713a1e50a3d6c0f
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-11-06 12:54:46 +0100

    rm

--- installer/prepare.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-@rem Prepare the build with a Lua Script
-luajit multitool/prepare.lua
-
-@rem Build the NSIS Installer
-makensis installer/installer.nsi
--- multitool/wrapperbin/msg-formatter.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-@REM Shortcut to start the multitool
-
-cd /d "%~dp0"
-"../msg-formatter/start.bat" %*
